09dc6db957ee6f52aaf7b9d3ea8fb1d5fe4fdce6
JasonHorsley2 <jasonhorsley2@gmail.com>
2016-02-18 12:29:33 -0500

    Reader account registration FINISHED, next is sessions at index.php (switch from HTML) and  My Books page

--- php/mainClasses/errorLookup.php
+++ php/mainClasses/errorLookup.php
@@ -8,6 +8,15 @@
  */
 class ErrorLookup
 {
+    private $devContactURL = "www.pubbly.com/console";
+    private $devContactHTML = false /*'<a href=' . $this . '->devContactURL>Jason</a>'*/;
+    /* TODO: FRIG!!
+     * Can't have an evaluated expression in an array declaration
+     * which means "message" => "contact $devContactHTML" won't freaking fracking work
+     *
+     * Maybe replace {devContact} with the private var in the constructor?
+     * I dunno man, but I aint doing it right now.
+     */
     private $errorKey = [
         1 => [
             "title" => "Error not found",
@@ -17,6 +26,7 @@ class ErrorLookup
             "title" => "HTML Template not found",
             "message" => "HTML Template requested in php/mainClasses/HTMLTemplate.php was not found in the key array.",
         ],
+        // 500 block is Registration errors
         500 => [
             "title" => "Registration Error:",
             "message" => "Invalid password configuration.",
@@ -41,16 +51,40 @@ class ErrorLookup
             "title" => "Registration Error:",
             "message" => "The email address you entered is not valid.",
         ],
+        // 510 block is Login errors
+        510 => [
+            "title" => "Login Error:",
+            "message" => "Missing Username or Password.",
+            "url" => "login",
+        ],
+        511 => [
+            "title" => "Login Error:",
+            "message" => "Wrong password.",
+            "url" => "login",
+        ],
+        512 => [
+            "title" => "Developer error:",
+            "message" => "Invalid password configuration, report to Jason",
+        ],
+        513 => [
+            "title" => "Login error:",
+            "message" => "Too many attempts, try against tomorrow or contact your system admin",
+            "url" => "login",
+        ]
 
     ];
-    public function lookUp($num) {
+
+    public function lookUp($num)
+    {
         $errorObj = isset($this->errorKey[$num]) ? $this->errorKey[$num] : false;
         if ($errorObj) {
             return $errorObj;
-        }   else    {
+        } else {
             return $this->errorKey[1];
         }
     }
-    function ErrorLookup() {
+
+    function ErrorLookup()
+    {
     }
 }
